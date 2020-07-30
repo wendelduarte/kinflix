ba0fe5ec88209ab175e0ffc53d734b019170fb1e
Wendel Duarte <wendel_duarte@outlook.com>
2020-07-30 19:12:42 -0300

    Fix environment

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,9 +29,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = window.location.hostname.includes('localhost') 
-      ? 'http://localhost:8080/categorias'
-      : 'https://kinflix.herokuapp.com/categorias';
+      const URL = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://kinflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
